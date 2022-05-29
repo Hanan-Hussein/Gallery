f46428b921c10abcdd73416971f7b0ee74f2f70d
Hanan-Hussein <hanhuss035@gmail.com>
2022-05-29 13:49:56 +0300

    ft:location and category-added location and the category models

--- album/models.py
+++ album/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    "Model stores location information in db"
+    name=models.CharField(max_length=60)
 
+class Category(models.Model):
+    """
+    Model stores category types in db
+    """
+    name=models.CharField(max_length=60)
 class Image(models.Model):
 
     """
@@ -10,4 +18,7 @@ class Image(models.Model):
     name=models.CharField(max_length=60)
     description=models.CharField(max_length=100)
     photo=models.ImageField(upload_to='album/',default='static/landing.png')
+    location=models.ForeignKey(Location, on_delete=models.CASCADE)
+    category=models.ForeignKey(Category, on_delete=models.CASCADE)
+
 
