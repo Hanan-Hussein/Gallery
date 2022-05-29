c036377e16d1dca96af723d46b9a4a5a53148fe7
Hanan-Hussein <hanhuss035@gmail.com>
2022-05-29 13:11:31 +0300

    ft:Image model-added image model

--- album/models.py
+++ album/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+
+    """
+    Model that stores images in a database
+    """
+    name=models.CharField(max_length=60)
+    description=models.CharField(max_length=100)
+    photo=models.ImageField(upload_to='album/',default='static/landing.png')
+
--- gallery/settings.py
+++ gallery/settings.py
@@ -78,10 +78,16 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER':'hanan',
+        'PASSWORD':'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
