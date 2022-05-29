af72cb1cc76364b34e780314d71944b7da8796e1
Hanan-Hussein <hanhuss035@gmail.com>
2022-05-29 22:54:52 +0300

    ft:display-showed the images in landing page

--- album/views.py
+++ album/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import Http404, HttpResponse
-
-
+from .models import Image
 # Create your views here.
 
 def welcome(request):
-    return render(request,'index.html')
+    photos_album=Image.objects.all()
+    
+    return render(request,'index.html',{"photos_album":photos_album})
