f188cabc4ac036168ac7819afc6908d5556265fa
Hanan-Hussein <hanhuss035@gmail.com>
2022-05-29 18:57:00 +0300

    ft:admin- added models to admiin dashboard

--- album/admin.py
+++ album/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Image, Location, Category
 # Register your models here.
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display=("name","description","location","category")
+    list_filter=("location","category")
+
+
+admin.site.register(Image, AlbumAdmin)
+admin.site.register(Location)
+admin.site.register(Category)
--- album/models.py
+++ album/models.py
@@ -1,24 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Location(models.Model):
     "Model stores location information in db"
-    name=models.CharField(max_length=60)
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
 
 class Category(models.Model):
     """
     Model stores category types in db
     """
-    name=models.CharField(max_length=60)
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+    
+
 class Image(models.Model):
 
     """
     Model that stores images in a database
     """
-    name=models.CharField(max_length=60)
-    description=models.CharField(max_length=100)
-    photo=models.ImageField(upload_to='album/',default='static/landing.png')
-    location=models.ForeignKey(Location, on_delete=models.CASCADE)
-    category=models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=60)
+    description = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='album/', default='static/landing.png')
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"Photo: {self.name} : description: {self.description} : location: {self.location.name} : category: {self.category.name}"
 
+    def delete_photo(self):
+        self.delete()
+
+    def save_photo(self):
+        self.save()
