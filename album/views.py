07d591eacb8da9a766334b18631360a4227008d5
Hanan-Hussein <hanhuss035@gmail.com>
2022-05-29 11:18:44 +0300

    st:nav-added a navbar to the page

--- album/views.py
+++ album/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import Http404, HttpResponse
+
 
 # Create your views here.
+
+def welcome(request):
+    return render(request,'index.html')
