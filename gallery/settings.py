3e08546c3e375d6d252b4ccdcb303c716bc3786a
Hanan-Hussein <hanhuss035@gmail.com>
2022-05-31 22:57:52 +0300

    deployment

--- gallery/settings.py
+++ gallery/settings.py
@@ -11,23 +11,27 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from pathlib import Path
 import os
-import dj_database_url
 import django_on_heroku
+import dj_database_url
+from decouple import config,Csv
+# from djangoconfig import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-
+MODE=config("MODE", default="dev")
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('*')
+ALLOWED_HOSTS = ['<your-app-name>.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
@@ -83,12 +87,12 @@ if config('MODE')=="dev":
 
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_pyscope2',
+            'ENGINE': 'django.db.backends.postgresql',
             'NAME': config('DB_NAME'),
             'USER':config('DB_USER'),
             'PASSWORD':config('DB_PASSWORD'),
             'HOST': config('DB_HOST'),
-            'PORT': '5432',
+            'PORT': '',
         }
     }
 else:
