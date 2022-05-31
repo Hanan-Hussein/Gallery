985f058856f28d21b1de91d82a1cda3087923a50
Hanan-Hussein <hanhuss035@gmail.com>
2022-05-31 17:05:51 +0300

    ft:deploy-adding deployment

--- gallery/settings.py
+++ gallery/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import django_on_heroku
+import django_heroku
 from pathlib import Path
 import os
 from decouple import config,Csv
@@ -149,7 +149,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 # Configure Django App for Heroku.
-django_on_heroku.settings(locals())
+django_heroku.settings(locals())
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 # Default primary key field type
